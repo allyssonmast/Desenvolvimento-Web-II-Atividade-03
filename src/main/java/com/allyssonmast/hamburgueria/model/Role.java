665acad704f4fc5ffd81e6e52e783f8fbd5f2ddc
allyssonmastrangelo <allysson-m@hotmail.com>
2026-05-02 17:14:31 -0300

    Croaçao das roles

---
+++ src/main/java/com/allyssonmast/hamburgueria/model/Role.java
@@ -0,0 +1,7 @@
+package com.allyssonmast.hamburgueria.model;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_MANAGER,
+    ROLE_ATTENDANT
+}
---
+++ src/main/java/com/allyssonmast/hamburgueria/model/Usuario.java
@@ -0,0 +1,30 @@
+package com.allyssonmast.hamburgueria.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
